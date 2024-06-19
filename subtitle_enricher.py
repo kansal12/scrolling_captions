bbeb1c89851c9fc9b618ee8afbb9958cb659bf7f
vk <vaibhavkansel@gmail.com>
2024-06-19 01:45:19 -0700

    Minor update in subtitle_enhancer

--- subtitle_enricher.py
+++ subtitle_enricher.py
@@ -4,6 +4,24 @@ with open("vocals.ass", encoding='utf_8_sig') as f:
     doc = ass.parse(f)
 
 print (len(doc.events))
+print (doc.events[0].text)
+
+def check_event_ends_with_brace(doc, i):
+    if 0 <= i < len(doc.events):
+        return doc.events[i].text.endswith('}')
+    else:
+        raise IndexError("Index out of range")
+
+result=check_event_ends_with_brace(doc,10)
+print(result)
+
+
+'''
+for i in range(1, len(doc.events)):
+            j=0
+            if (doc.events[i].text)
+            previous_text = doc.events[i-1].text            
+            current_text = doc.events[i].text
 
 #print(re.sub(r'\{[^}]*\}', '', doc.events[1].text))
 
@@ -25,7 +43,7 @@ def find_closest_brace(lines):
     return closest_brace_lines
 
 1. check previous line, if it ends with }
-    a. No previous line- concat (/n, current line)If there is no prevous line, 
-    b. Previous lines does not end with }, then  
+    a. No previous line ending with }- concat (/n, current line)
+    b. Previous lines ends with }, then concat(previous line, /n, current line)  
 
-
+'''
